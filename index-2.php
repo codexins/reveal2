d6ed5da450c7d4398e48359eccf458cdfefb8ccd
codexmrashid <mrashid@codexin.com>
2017-08-14 11:49:39 +0600

    little change

--- index-2.php
+++ index-2.php
@@ -46,30 +46,33 @@
 	<header class="header  rv2">
 		<nav class="navbar" data-spy="affix" data-offset-top="150">
 			<div class="container">
-				<!-- Brand and toggle get grouped for better mobile display -->
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#reveal-navbar-collapse-1" aria-expanded="false">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="http://themeitems.com/tf/reveal-preview/"><img src="rev-2-img/logo.png" alt=""></a>
-				</div>
-
-				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse" id="reveal-navbar-collapse-1">
-					<ul class="nav navbar-nav navbar-right main-menu">
-						<li><a href=".header">home</a></li>
-						<li><a href="#story">Abouth</a></li>
-						<li><a href="#services">Services</a></li>
-						<li><a href="#events_rv2">Events </a></li>
-						<li><a href="#portfolio_rv2">Portfolio</a></li>
-						<li><a href="#letest_post">Blog</a></li>
-						<li><a href="#location">Contact</a></li>
-						<li><div class="search-icon"><i class="fa fa-search" aria-hidden="true"></i></div></li>
-					</ul>
-				</div><!-- /.navbar-collapse -->
+				<div class="row">
+					<div class="col-sm-12">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#reveal-navbar-collapse-1" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="http://themeitems.com/tf/reveal-preview/"><img src="rev-2-img/logo.png" alt=""></a>
+						</div>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="reveal-navbar-collapse-1">
+							<ul class="nav navbar-nav navbar-right main-menu">
+								<li><a href=".header">home</a></li>
+								<li><a href="#story">Abouth</a></li>
+								<li><a href="#services">Services</a></li>
+								<li><a href="#events_rv2">Events </a></li>
+								<li><a href="#portfolio_rv2">Portfolio</a></li>
+								<li><a href="#letest_post">Blog</a></li>
+								<li><a href="#location">Contact</a></li>
+								<li><div class="search-icon"><i class="fa fa-search" aria-hidden="true"></i></div></li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- col-sm-12 -->
+				</div><!-- row-->
 			</div><!-- /.container-fluid -->
 		</nav> <!-- end of nav -->
 		<!-- slider section  -->
